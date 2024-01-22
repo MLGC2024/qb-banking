f7d582171c9502c141a72aed7c5c841ac563c6e2
GamerGirlie79 <96111614+GamerGirlie79@users.noreply.github.com>
2024-01-22 14:44:49 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {
     useTarget = GetConvar('UseTarget', 'false') == 'true',
-    atmModels = { 'prop_atm_01', 'prop_atm_02', 'prop_atm_03', 'prop_fleeca_atm' },
+    atmModels = { 'prop_atm_01', 'prop_atm_02', 'prop_atm_03', 'prop_fleeca_atm', 'prop_atm_01' },
     useDailyLimit = true,
     dailyLimit = 5000,
     maxAccounts = 2,
